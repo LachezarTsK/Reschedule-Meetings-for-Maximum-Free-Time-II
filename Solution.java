8668c2c86b3570d64b77db23e93714b2f62c81c3
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-07-12 18:10:31 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,43 @@
+
+public class Solution {
+
+    public int maxFreeTime(int eventTime, int[] startEventTime, int[] endEventTime) {
+        int numberOfEvents = startEventTime.length;
+
+        int[] forwardMaxFreeTime = new int[numberOfEvents + 1];
+        forwardMaxFreeTime[0] = startEventTime[0];
+        for (int i = 1; i < numberOfEvents; ++i) {
+            forwardMaxFreeTime[i] = Math.max(forwardMaxFreeTime[i - 1], startEventTime[i] - endEventTime[i - 1]);
+        }
+
+        int[] backwardMaxFreeTime = new int[numberOfEvents + 1];
+        backwardMaxFreeTime[backwardMaxFreeTime.length - 1] = eventTime - endEventTime[numberOfEvents - 1];
+        for (int i = numberOfEvents - 1; i > 0; --i) {
+            backwardMaxFreeTime[i] = Math.max(backwardMaxFreeTime[i + 1], startEventTime[i] - endEventTime[i - 1]);
+        }
+
+        int maxContinuousFreeTime = 0;
+        for (int i = 0; i < numberOfEvents; ++i) {
+            int startAdjacentFreeTimeBeforeEvent = (i - 1) >= 0 ? endEventTime[i - 1] : 0;
+            int endAdjacentFreeTimeAfterEvent = (i + 1) < numberOfEvents ? startEventTime[i + 1] : eventTime;
+
+            int maxPreceedingNonadjacentFreeTime = (i - 1 >= 0) ? forwardMaxFreeTime[i - 1] : 0;
+            int maxFollowingNonadjacentFreeTime = (i + 2 <= backwardMaxFreeTime.length - 1) ? backwardMaxFreeTime[i + 2] : 0;
+            int maxNonadjacentFreeTime = Math.max(maxPreceedingNonadjacentFreeTime, maxFollowingNonadjacentFreeTime);
+
+            if (maxNonadjacentFreeTime < endEventTime[i] - startEventTime[i]) {
+                maxContinuousFreeTime = Math.max(maxContinuousFreeTime,
+                                                 endAdjacentFreeTimeAfterEvent
+                                               - startAdjacentFreeTimeBeforeEvent
+                                               - (endEventTime[i] - startEventTime[i]));
+                continue;
+            }
+
+            maxContinuousFreeTime = Math.max(maxContinuousFreeTime,
+                                             endAdjacentFreeTimeAfterEvent
+                                           - startAdjacentFreeTimeBeforeEvent);
+        }
+
+        return maxContinuousFreeTime;
+    }
+}
